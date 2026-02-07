8cfbb0d04c74006d49e411be96e90dd5f38a3a83
Nipun Theekshana <theekshana2@gmail.com>
2026-02-07 17:59:10 +0100

    refactor: update ApiResponse struct to use static message strings and improve visibility

--- api/src/response.rs
+++ api/src/response.rs
@@ -2,26 +2,18 @@ use actix_web::HttpResponse;
 use serde::Serialize;
 
 #[derive(Serialize)]
-pub struct ApiResponse<T: Serialize> {
+pub(crate) struct ApiResponse<T: Serialize> {
     pub code: u16,
-    pub message: String,
+    pub message: &'static str,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub payload: Option<T>,
 }
 
 impl<T: Serialize> ApiResponse<T> {
     pub fn ok(payload: T) -> HttpResponse {
-        HttpResponse::Ok().json(ApiResponse {
+        HttpResponse::Ok().json(Self {
             code: 200,
-            message: "success".to_string(),
-            payload: Some(payload),
-        })
-    }
-
-    pub fn created(payload: T) -> HttpResponse {
-        HttpResponse::Created().json(ApiResponse {
-            code: 201,
-            message: "created".to_string(),
+            message: "success",
             payload: Some(payload),
         })
     }
