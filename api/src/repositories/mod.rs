260fde6066b0a41ab3dbcd611f4e6c2c75dd8723
Nipun Theekshana <theekshana2@gmail.com>
2026-03-03 16:23:09 +0100

    feat: add StatsRepository module to repositories

--- api/src/repositories/mod.rs
+++ api/src/repositories/mod.rs
@@ -1,7 +1,9 @@
 pub(crate) mod country;
 pub(crate) mod geocoding;
 pub(crate) mod population;
+pub(crate) mod stats;
 
 pub(crate) use country::CountryRepository;
 pub(crate) use geocoding::GeocodingRepository;
 pub(crate) use population::PopulationRepository;
+pub(crate) use stats::StatsRepository;
